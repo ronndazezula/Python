05b5cfb042e2a5970e80336c8c2fc2fd2070d96d
Ronnda Zezula <zezula.ronnda@gmail.com>
2020-06-09 09:07:52 -0700

    Update files
    Resubmit Step 174 Assignment

--- Step174_ClassAssign.py
+++ Step174_ClassAssign.py
@@ -1,3 +1,7 @@
+"""This is a very loose representation of an email and two associated files to
+represent two classes that inherit from another class for Assignment Step 174.
+The attributes are a loose representation of file metadata, not intended to link."""
+
 class Email:
 	dateRecd = 19991231
 	recipName = 'John Doe'
@@ -8,17 +12,43 @@ class Email:
 
 class Attach(Email):
 	doctype = 'PowerPoint PPT Presentation'
-	fileName = 'CuteCats.ppt'
-	filePath = 'C:\JD\Documents\CuteCats.ppt'
+	fileName = 'Step174_ClassAssign-img1.jpg'
+	filePath = "C:\\Users\\ronnd\\Pictures\\img\\Step174_ClassAssign-img1.jpg"
+	MD5Hash = '1f9b28cb3b83ade5228589642efb12dd'
 
 class Embed(Email):
 	doctype = 'Graphics Interchange Format'
-	fileName = 'Kitties.gif'
-	filePath = 'C:\JD\Pictures\Kitties.gif'
+	fileName = 'Step174_ClassAssign-img2.gif'
+	filePath = "C:\\Users\\ronnd\\Pictures\\img\\Step174_ClassAssign-img2.gif"
+	MD5Hash = 'fed3ec9946f427464b1a9f53e59af1b3'
 
 print('Date Cat Content Received:')
 print(Email.dateRecd)
-print('\nCat Attachment:')
-print(Attach.fileName)
-print('\nCat Embedded Image:')
-print(Embed.fileName)
+
+print('\nCat Attachment Native Path:')
+print(Attach.filePath)
+
+print('\nCat Embedded Image Native Path:')
+print(Embed.filePath)
+
+from pathlib import Path
+import webbrowser
+import os
+
+filename = Path(".\img\Step174_ClassAssign-img1.jpg")
+
+iexplore = os.path.join(os.environ.get("PROGRAMFILES", "C:\\Program Files"),
+                        "Internet Explorer\\IEXPLORE.EXE")
+
+browser = webbrowser.get(iexplore)
+
+browser.open(filename.absolute().as_uri())
+
+filename = Path(".\img\Step174_ClassAssign-img2.gif")
+
+iexplore = os.path.join(os.environ.get("PROGRAMFILES", "C:\\Program Files"),
+                        "Internet Explorer\\IEXPLORE.EXE")
+
+browser = webbrowser.get(iexplore)
+
+browser.open(filename.absolute().as_uri())
